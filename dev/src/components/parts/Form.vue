bc2c29821c2e273951c577a46d4a006c01b61a83
Lensom <magurawork@gmail.com>
2020-04-21 20:17:59 +0300

    update package

--- dev/src/components/parts/Form.vue
+++ dev/src/components/parts/Form.vue
@@ -1,18 +1,18 @@
 <template>
   <form class="form">
     <div class="field name-box">
-        <input class="form__input" type="text" id="name" placeholder="Who Are You?"/>
-        <label for="name">Name</label>
+      <input class="form__input" type="text" id="name" placeholder="Who Are You?" />
+      <label for="name">Name</label>
     </div>
 
     <div class="field email-box">
-        <input class="form__input" type="text" id="email" placeholder="Your email"/>
-        <label for="email">Email</label>
+      <input class="form__input" type="text" id="email" placeholder="Your email" />
+      <label for="email">Email</label>
     </div>
 
     <div class="field msg-box">
-        <textarea class="form__textarea" id="msg" rows="4" placeholder="Your message goes here..."/></textarea>
-        <label for="msg">Msg</label>
+      <textarea class="form__textarea" id="msg" rows="4" placeholder="Your message goes here..."></textarea>
+      <label for="msg">Msg</label>
     </div>
 
     <input class="button btn--submit" type="submit" value="Send" />
@@ -20,7 +20,5 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>  
